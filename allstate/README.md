f35b402161ebaa0a38c4b1a22184a4cc57cbb386
wangshihan <258200029@qq.com>
2017-07-02 12:12:57 +0800

    first version

---
+++ allstate/README.md
@@ -0,0 +1,5 @@
+# kaggle competition
+
+date=20170701
+url=https://www.kaggle.com/c/allstate-claims-severity
+
---
+++ allstate/src/load_data.py
@@ -0,0 +1,18 @@
+#coding=utf8
+
+"""
+This file contains methods to load train.csv and test.csv
+
+"""
+
+
+def load_train(filename):
+
+
+
+def main():
+    print 'load data test!'
+
+
+if __name__ == '__main__':
+    main()
